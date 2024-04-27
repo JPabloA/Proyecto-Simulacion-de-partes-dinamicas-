c9b0a634fcee98ed2b4e45e76997abec97bed7ed
DeynerNB <deynernavarro@estudiantec.cr>
2024-04-26 20:11:55 -0600

    Inicializer

--- initializer.c
+++ initializer.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/shm.h>
-#include <semaphore.h>
 #include <unistd.h>
-#include <utilities.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "utilities.h"
 
-#define SHM_KEY 0x12345
-#define SEM_KEY 0x54321
+#define FILENAME "./sharedFile"
 
-int main() {
+int main(int argc, char const *argv[]) {
     int num_lines;
     printf("Enter the number of memory lines: ");
     scanf("%d", &num_lines);
@@ -17,39 +16,41 @@ int main() {
     int line_size = sizeof(Line); // * sizeof let me know the space that its needed for one line
     int memory_size = num_lines * line_size;
 
-    // Create shared memory for memory simulation
-    int shmid = shmget(SHM_KEY, memory_size, IPC_CREAT | 0666);
-    if (shmid == -1) {
-        perror("Error creating shared memory");
+    // Set the key to the memory segment
+    key_t key = ftok(FILENAME, 's');
+    int shmid;
+
+    // Create shared memory by the size of the struct MemoryCell
+    shmid = shmget(key, memory_size, IPC_CREAT | 0666);
+    if (shmid < 0) {
+        perror("shmget");
+        exit(1);
+    }
+
+    // Attach shared memory to the process
+    Line* memory = (Line*) shmat(shmid, NULL, 0);
+    if (memory == (Line*) -1) {
+        perror("shmat");
         exit(1);
     }
 
     // Initialize the shared memory
-    Line *memory = shmat(shmid, NULL, 0);
     for (int i = 0; i < num_lines; i++) {
         memory[i].state = Available;
         memory[i].pid = -1;
         memory[i].time = 0;
     }
 
-    // Create semaphores for synchronization
-    sem_t *sem_memory = sem_open("/sem_memory", O_CREAT | O_EXCL, 0644, 1);
-    sem_t *sem_log = sem_open("/sem_log", O_CREAT | O_EXCL, 0644, 1);
+    printf("Memory shared with ID: %d and %d bytes\n", shmid, memory_size);
+    sleep(5);
 
-    // Open the log file
-    FILE *log_file = fopen("log.log", "w");
-    if (log_file == NULL) {
-        perror("Error opening log file");
+    // Free shared memory (The segment is not available JUST for this process anymore)
+    if (shmdt(memory) == -1) {
+        perror("shmdt");
         exit(1);
     }
 
-    // Print initialization information
-    printf("Shared memory and semaphores initialized.\n");
-    fprintf(log_file, "Simulation started.\n");
-    fprintf(log_file, "Number of memory lines: %d\n", num_lines);
-    fprintf(log_file, "Allocated memory size: %d bytes\n", memory_size);
+    printf("Shared memory segment free and released\n");
 
-    // Close the log file and terminate the process
-    fclose(log_file);
-    exit(0);
+    return 0;
 }
