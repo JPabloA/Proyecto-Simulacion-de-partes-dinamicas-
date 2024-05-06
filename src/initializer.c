58dfdc986b8d226a8cfc90c715ea4ba7c59bccf4
DeynerNB <deynernavarro@estudiantec.cr>
2024-05-05 22:44:30 -0600

    Added process list initializer

--- src/initializer.c
+++ src/initializer.c
@@ -7,8 +7,9 @@
 #include <sys/stat.h> /* For mode constants */
 #include <semaphore.h>
 
-#include "utilities.h"
-#include "sharedMemory.h"
+#include "./utilities/utilities.h"
+#include "./utilities/sharedMemory.h"
+#include "./utilities/process_list.h"
 
 sem_t semaphoreMemory;
 
@@ -31,12 +32,46 @@ void initMemoryLines(int shmid, int num_lines)
 
     printf("Filling memory lines...\n");
     for (int i = 0; i < num_lines; ++i) {
+        lines[i].state = InUse;
+        lines[i].pid = 99;
+    }
+    // *** Para Worst Fit
+    // for (int i = 1; i < 5; ++i) {
+    //     lines[i].state = Available;
+    //     lines[i].pid = -1;
+    // }
+    // for (int i = 7; i < num_lines; ++i) {
+    //     lines[i].state = Available;
+    //     lines[i].pid = -1;
+    // }
+
+    // *** Para Best Fit
+    for (int i = 0; i < 8; ++i) {
+        lines[i].state = Available;
+        lines[i].pid = -1;
+    }
+    for (int i = 9; i < num_lines; ++i) {
         lines[i].state = Available;
         lines[i].pid = -1;
     }
     printf("Memory lines filled!\n");
 
     detachSharedMemorySegment(lines);
+}
+
+void initProcessList(int shmid) {
+    printf("\n");
+    Process_List *list = (Process_List*)attachSharedMemorySegment(shmid);
+
+    printf("Filling process list by default...\n");
+    for (int i = 0; i < MAX_LIST_LENGTH; i++) {
+        list[i].proc = NULL;
+        list[i].proc_state = NOT_DEFINED;
+        list[i].state = EMPTY;
+    }
+    printf("Process list initialized!\n");
+
+    detachSharedMemorySegment(list);
 }
 
 int main(int argc, char const *argv[])
@@ -57,9 +92,11 @@ int main(int argc, char const *argv[])
 
     int shmid1 = createSharedMemorySegment(FILENAME, 's', memory_size);
     int shmid2 = createSharedMemorySegment(SHARED_INFO, 'a', sizeof(SharedInformation));
+    int shmid3 = createSharedMemorySegment(PROC_FILE, 'b', sizeof(Process_List) * MAX_LIST_LENGTH);
 
     initMemoryLines(shmid1, num_lines);
     initSharedInformation(shmid2, num_lines);
+    initProcessList(shmid3);
 
     printf("\n > All shared memory segments set\n\n");
 
