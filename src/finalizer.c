7bd87d902d35d606f1f90328f766a5dd0298fc68
DeynerNB <deynernavarro@estudiantec.cr>
2024-05-06 15:00:02 -0600

    Added sem_wait and sem_post

--- src/finalizer.c
+++ src/finalizer.c
@@ -18,16 +18,21 @@ int main(int argc, char const *argv[]) {
     sem_t* semaphoreMemory = GetSemaphore(SNAME);
     sem_t* semaphoreProcList = GetSemaphore(SNAME_PROC_LIST);
 
-    if (semaphoreMemory == NULL || semaphoreProcList == NULL) {
-        printf("ERROR: Failed getting semaphore - Finalizer\n");
-        return 1;
+    if (semaphoreMemory == NULL) {
+        printf("ERROR: Failed getting memory semaphore - Finalizer\n");
+    }
+    else {
+        CloseSemaphore(semaphoreMemory);
+        UnlinkSemaphore(SNAME);
     }
 
-    CloseSemaphore(semaphoreMemory);
-    CloseSemaphore(semaphoreProcList);
-
-    UnlinkSemaphore(SNAME);
-    UnlinkSemaphore(SNAME_PROC_LIST);
+    if (semaphoreProcList == NULL) {
+        printf("ERROR: Failed getting proc list semaphore - Finalizer\n");
+    }
+    else {
+        CloseSemaphore(semaphoreProcList);
+        UnlinkSemaphore(SNAME_PROC_LIST);
+    }
 
     return 0;
 }
